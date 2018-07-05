4023d820f3a391492613ae6287accd6d58a89ebd
vagrant <vagrant@localhost.localdomain>
2018-07-05 19:21:13 +0900

    add resarch API

--- main.rb
+++ main.rb
@@ -2,8 +2,17 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra'
 require 'sinatra/reloader'
+require 'uri'
+require 'net/http'
 
 
 get '/' do
+  base_url = 'http://wikipedia.simpleapi.net/api?output=html&keyword='
+  if params[:keyword]
+    keyword = params[:keyword]
+    url = URI.parse(base_url + keyword)
+    @result = Net::HTTP.get(url).force_encoding('utf-8')
+  end
+
   erb :main
 end
